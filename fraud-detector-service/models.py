2505b7e54b12b30aa7ca2a44281d7f7aad822506
David Veszelovszki <veszelovszki@gmail.com>
2025-04-23 14:43:22 +0200

    Add comments for server fields

--- fraud-detector-service/models.py
+++ fraud-detector-service/models.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 # Define the output structure for fraud detection
 class FraudDetection(BaseModel):
-    fraud: int
-    score: float
-    comment: str
+    fraud: int # 1 if it's likely fraud, 0 otherwise.
+    score: float # A float from 0.0 to 1.0 indicating confidence of fraud likelihood.
+    comment: str # A short explanation justifying the assessment.
 
 # Define the input structure expected by the API handler
 class FraudRequest(BaseModel):
